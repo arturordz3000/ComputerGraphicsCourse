153b13166aecced9fa883978afb74ea08d1fc577
Arturo Rodríguez <arturo.rdz3000@gmail.com>
2016-05-11 12:39:01 -0500

    Added project generator script prototype for XCode and Visual Studio

---
+++ GenerateProjectFiles.js
@@ -0,0 +1,9 @@
+var  projectGenerator = require('./ProjectGeneration/ProjectGenerator');
+
+var projects = [
+	'BresenhamLineAlgorithm',
+	'CourseCore',
+	'DDALineAlgorithm'
+];
+
+projectGenerator.generate(projects);
